af6d853667a8aa23d2f5645bd5c431bd9a18e0e4
ericajackson8 <56225030+ericajackson8@users.noreply.github.com>
2023-03-11 14:46:58 -0500

    deliverable 3 and all pics

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,3 +20,25 @@ View(total_summary)
 group_data <- group_by(suspension_coil,Manufacturing_Lot)
 lot_summary <- summarize(group_data,mean = mean(PSI), median = median(PSI), variance = var(PSI), SD=sd(PSI) )
 View(lot_summary)
+
+# Deliverable 3
+#Mean Across All Lots Compared to Population Mean of 1500
+total_mean <- subset(suspension_coil,select=PSI)
+t.test(total_mean, alternative="two.sided", mu=1500)
+t.test(subset(suspension_coil,select=PSI), alternative="two.sided", mu=1500)
+
+# Mean of Lot 1 PSI Compared to Population Mean of 1500
+#lot1_mean <- subset(suspension_coil,Manufacturing_Lot=="Lot1",select=PSI)
+#t.test(lot1_mean,total_mean)
+#t.test(lot1_mean, alternative="two.sided", mu=1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1",select=PSI), alternative="two.sided", mu=1500)
+
+# Mean of Lot 2 PSI Compared to Population Mean of 1500
+#lot2_mean <- subset(suspension_coil,Manufacturing_Lot=="Lot2",select=PSI)
+#t.test(lot2_mean, total_mean)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2",select=PSI), alternative="two.sided", mu=1500)
+
+# Mean of Lot 3 PSI Compared to Population Mean of 1500
+#lot3_mean <- subset(suspension_coil,Manufacturing_Lot=="Lot3",select=PSI)
+#t.test(lot3_mean, total_mean)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3",select=PSI), alternative="two.sided", mu=1500)
